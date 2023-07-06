6faa8e47bfaa516a66566c66c030bf40ee949038
egorb <egorbaturin2013@yandex.ru>
2023-07-06 11:32:46 +0300

    Пилим сервис картинок

--- file_manager/src/main/java/org/trip/files/controller/FileUploadController.java
+++ file_manager/src/main/java/org/trip/files/controller/FileUploadController.java
@@ -1,46 +1,28 @@
 package org.trip.files.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
-import org.springframework.util.StreamUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.trip.files.service.FileUploadService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.Part;
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 
 @RestController
-@Slf4j
 public class FileUploadController {
+    @Autowired
+    private FileUploadService fileUploadService;
 
     @GetMapping("/upload")
     public @ResponseBody String provideUploadInfo(){
         return "Вы можете загружать файл с использование этой ссылки";
     }
 
-    @RequestMapping(value = "/upload", method = RequestMethod.POST,
-        consumes = {"multipart/form-data"})
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = {"multipart/form-data"})
     public @ResponseBody String handleFileUpload(HttpServletRequest request) throws ServletException, IOException {
-        for (Part part : request.getParts()){
-            String contentType = part.getContentType();
-            String parameterName = part.getName();
-            String filename = part.getSubmittedFileName();
-            log.info("Content type: " + contentType);
-            log.info("Parameter name: " + parameterName);
-            log.info("File name: " + filename);
-
-            try (InputStream inputStream = part.getInputStream()){
-                byte[] content = StreamUtils.copyToByteArray(inputStream);
-                Files.write(Path.of("/upload/"),
-                        content);
-                log.info("file has been stored");
-            }
-        }
+        fileUploadService.uploadFile(request);
         return "success";
-
     }
 
 
